78b045877c2249f65b2cf3a43a3e39492dfae4aa
yeon3ee <2310538@sookmyung.ac.kr>
2023-10-19 16:11:04 +0900

    05

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void f(void);
-
-int i;
+void sub(void);
 int main(void)
 {
-	for(i=0;i<5;i++)
-	{
-		f();
-	}
+	int i;
+	for(i=0;i<3;i++)
+		sub();
 	return 0;
 }
 
-void f(void)
+void sub(void)
 {
-	for(i=0;i<10;i++)
-		printf("#");
+	int auto_count=0;
+	static int static_count = 0;
+	auto_count++;
+	static_count++;
+	printf("auto_count=%d\n",auto_count);
+	printf("static_count=%d\n",static_count);
 }
