561db0582d8a8110bdd34849c5c2ac3b1308daa0
yeon3ee <2310538@sookmyung.ac.kr>
2023-10-19 15:49:51 +0900

    04

--- main.c
+++ main.c
@@ -3,19 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int counter;
+
+void f(void);
+
+int i;
 int main(void)
-	
 {
-	int i=10;//지역변수임 
-	printf("함수 호출전 i=%d\n",i);
-	i=inc(i);
-	printf("함수 호출후i=%d\n",i);
+	for(i=0;i=5;i++)
+	{
+		f();
+	}
 	return 0;
 }
 
-int inc(int counter)  
+void f(void)
 {
-	counter++;
-	return counter;
+	for(i=0;i<10;i++)
+		printf("#");
 }
-
